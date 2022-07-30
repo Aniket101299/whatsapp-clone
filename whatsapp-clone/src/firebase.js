68feb918b829d7effb8a88a5e0ec771d10530d86
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-30 14:08:41 +0530

    divide whole app into 2 components Sidebar and Chat, created both of them and install all necessary dependencies like axios, material-ui, firebase

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCqYa_anijA7aq7LX_z4ukSB7SaS5-hwzc",
+    authDomain: "whatsapp-clone-a4359.firebaseapp.com",
+    projectId: "whatsapp-clone-a4359",
+    storageBucket: "whatsapp-clone-a4359.appspot.com",
+    messagingSenderId: "635716988827",
+    appId: "1:635716988827:web:158ae3541e37ff56263b3e",
+    measurementId: "G-HLE60VD5P6"
+  };
