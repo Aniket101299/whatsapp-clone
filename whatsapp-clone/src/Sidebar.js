f03e661bee2f037ba1cf5cbba8b13a750127d2cd
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-30 15:32:18 +0530

    completed sidebar chat successfully with random avatars & option to create new chat

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -5,6 +5,7 @@ import { Avatar, IconButton } from '@material-ui/core';
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { SearchOutlined } from "@material-ui/icons";
+import SidebarChat from './SidebarChat';
 
 
 function Sidebar() {
@@ -32,6 +33,11 @@ function Sidebar() {
         </div>
       </div>  
 
+      <div className='sidebar_chats'>
+        <SidebarChat addNewChat/>
+        <SidebarChat />
+        <SidebarChat />
+      </div>
 
     </div>
   )
